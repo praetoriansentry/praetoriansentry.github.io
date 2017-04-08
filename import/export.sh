b203bf1eea6593365cc165c43ae35b1f3177d672
John Hilliard <jhilliard@nextjump.com>
2017-04-08 18:03:42 +0000

    Adding import script

---
+++ import/export.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+curl 'http://hilliardquotes.tumblr.com/api/read/json?start=0&debug=1' | jq '.' > raw-data.0.json
+curl 'http://hilliardquotes.tumblr.com/api/read/json?start=20&debug=1' | jq '.' > raw-data.1.json
+curl 'http://hilliardquotes.tumblr.com/api/read/json?start=40&debug=1' | jq '.' > raw-data.2.json
+curl 'http://hilliardquotes.tumblr.com/api/read/json?start=60&debug=1' | jq '.' > raw-data.3.json
+
+cat *.json | jq --raw-output '.posts[] | [."unix-timestamp", ."quote-text", ."quote-source"] | @tsv' | sed 's/^"//' | sed 's/"$//' > data.tsv
+
+cat data.tsv | parallel --colsep "\t" "echo -n {2} > {1}.org"
+cat data.tsv | parallel --colsep "\t" "echo -e \"\n\nFrom:\n\" >> {1}.org"
+cat data.tsv | parallel --colsep "\t" "echo -n {3} >> {1}.org"
