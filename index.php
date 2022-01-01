1d8f383e86a03912f7c3022837113f256f0eb14e
John Hilliard <jhilliard@nextjump.com>
2022-01-01 11:22:15 -0500

    latest changes

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   
   <div class="row spacious">
     <div class="col-12">
-      <img src="img/comp/4R8A3566_md.jpg" class="img-fluid" title="Books I like">
+      <img src="img/comp/4R8A3566_md.jpg" class="img-fluid" title="Some of my programming / software books">
     </div>
   </div>
 
@@ -19,7 +19,7 @@
     <div class="col-12">
       <p class="bigtext">
         <a href="https://www.youtube.com/watch?v=rEq1Z0bjdwc">HELLO THERE.</a> <br />
-        My name is John. I am a software engineer. I enjoy coding, photography, movies, web3, security, etc...
+        My name is John. I am a software engineer. I enjoy <a href="https://github.com/praetoriansentry">coding</a>, <a href="https://opensea.io/collection/praetorianphoto">photography</a>, <a href="https://j4.is/movies/">movies</a>, web3, <a href="https://twitter.com/praetorian/thread/1416501549145133063">security</a>, etc...
       </p>
     </div>
   </div>
@@ -41,7 +41,7 @@
 
   <div class="row spacious">
     <div class="col-md-6 col-sm-16 col-sm-12">
-      <img src="img/comp/4R8A3546_sm.jpg" class="img-fluid" title="Arduinos">
+      <img src="img/comp/4R8A3546_sm.jpg" class="img-fluid" title="A random Arduino. I don't remember what I last used it for.">
     </div>
     <div class="col-md-6 col-sm-16 d-flex">
       <div class="align-self-center desctext">
@@ -58,7 +58,7 @@
   <div class="row spacious">
     <div class="col-12 d-flex">
       <div class="align-self-center desctext mb-5">
-        I also like finding vintage electronics like this Z-Angle Meter or the front panel of an IBM 1401
+        I also like finding vintage electronics like this Z-Angle Meter or the front panel of an <a href="https://en.wikipedia.org/wiki/IBM_1401">IBM 1401</a>
       </div>
     </div>
     <div class="col-md-6 col-sm-16">
@@ -75,7 +75,7 @@
   
   <div class="row spacious">
     <div class="col-md-6 col-sm-16">
-      <img src="img/comp/4R8A3552_sm.jpg" class="img-fluid" title="Electronics of the Z-Angle Meter">
+      <img src="img/comp/4R8A3552_sm.jpg" class="img-fluid" title="Internal electronics of the Z-Angle Meter">
     </div>
     <div class="col-md-6 col-sm-16 d-flex">
       <div class="align-self-center desctext">
@@ -107,7 +107,9 @@
 
   <div class="row spacious">
     <div class="col-md-6 col-sm-16">
-      <img src="img/kitty.png" class="img-fluid" title="Crypto Kitty">
+      <a href="https://www.cryptokitties.co/kitty/376648">
+        <img src="img/kitty.png" class="img-fluid" title="Crypto Kitty">
+      </a>
     </div>
     <div class="col-md-6 col-sm-16 d-flex">
       <div class="align-self-center desctext">
@@ -124,7 +126,9 @@
       </div>
     </div>
     <div class="col-md-6 col-sm-16">
-      <img src="img/blockbuster.jpg" class="img-fluid" title="Rubiks Cube">
+      <a href="https://foundation.app/@lelandkfoster/~/9646">
+        <img src="img/blockbuster.jpg" class="img-fluid" title="A blockbuster painting that made me nostalgic">
+      </a>
     </div>
   </div>
 
@@ -138,16 +142,28 @@
       I also like creating data visualizations.
     </div>
     <div class="col-md-6 col-sm-16">
-      <img src="img/binviz/viz1_sm.jpg" class="img-fluid" title="Data Viz 1">
+      <p class="caption">
+        A visual representation of a Markov tree based on web navigation data.
+      </p>
+      <img src="img/binviz/viz1_sm.jpg" class="img-fluid" title="A visual representation of a Markov tree based on web navigation data.">
     </div>
     <div class="col-md-6 col-sm-16">
+      <p class="caption">
+        Using community detection (<a href="https://en.wikipedia.org/wiki/Louvain_method">Louvain</a>) to segment users based on behavior.
+      </p>
       <img src="img/binviz/viz2_sm.jpg" class="img-fluid" title="Data Viz 2">
     </div>
     <div class="col-md-6 col-sm-16">
-      <img src="img/binviz/mona_sm.jpg" class="img-fluid" title="Data Viz 3">
+      <p class="caption">
+        A binary analysis / visualization of a high resolution capture of the Mona Lisa.
+      </p>
+      <img src="img/binviz/mona_sm.jpg" class="img-fluid" title="A binary analysis / visualization of a high resolution capture of the Mona Lisa.">
     </div>
     <div class="col-md-6 col-sm-16">
-      <img src="img/binviz/viz3_sm.jpg" class="img-fluid" title="Data Viz 4">
+      <p class="caption">
+        A network analysis of different brands to develop a distance metric.
+      </p>
+      <img src="img/binviz/viz3_sm.jpg" class="img-fluid" title="A network analysis of different brands to understand how they naturally relate to eachother.">
     </div>
 
   </div>
